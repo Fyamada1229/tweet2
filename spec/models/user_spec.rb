f48865781aa8a5f6ed13ca2e0db5f8dbe4bfe4b1
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-09-12 23:50:27 +0900

    test add

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  describe "#create" do
-    it "nicknameがない場合は登録できないこと" do
-      user = User.new(nickname: nil, email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+  describe '#create' do
+    
+    it "nikcnameがない場合は登録できないこと" do
+      user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
+
+    it "emailがないと場合は登録出来ないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
   end
 end
-
-
